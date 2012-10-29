85c80b42e3e3f0782066a45dbe828b1e66974be4
MlleIrene <mlleirenecahendanvers@gmail.com>
2012-10-28 22:45:53 -0700

    ・タブボタンにボタン名を追加 ・フードラインをTableViewControllerからViewControllerに変更

--- Meshi365/MSFoodLineViewController.h
+++ Meshi365/MSFoodLineViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MSFoodLineViewController : UIViewController
+@interface MSFoodLineViewController : UIViewController<UITableViewDataSource, UITableViewDelegate> 
+{
+	UITableViewStyle style; 
+	UITableView *tableView;
+	
+}
+
+@property (nonatomic, retain) UITableView *tableView;
+
+- (id)initWithStyle:(UITableViewStyle)theStyle;
 
 @end
--- Meshi365/MSFoodLineViewController.m
+++ Meshi365/MSFoodLineViewController.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation MSFoodLineViewController
+@synthesize tableView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +27,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +34,92 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)viewWillAppear:(BOOL)animated {
+	
+	
+	[super viewWillAppear:animated];
+	
+    // detail から master に戻ってきた場合に、選択された行のハイライトを消す。
+    NSIndexPath *indexPath = tableView.indexPathForSelectedRow;
+    if (indexPath != nil) {
+        [tableView deselectRowAtIndexPath:indexPath animated:animated];
+    }
+
+	
+}
+
+
+
+
+
+- (id)initWithStyle:(UITableViewStyle)theStyle
+{
+    self = [super init];    // xib ファイルは使わないと仮定。
+    if (self != nil) {
+        style = theStyle;
+    }
+    return self;
+}
+
+
+// loadView メソッドを追加。
+- (void)loadView
+{
+	UIView *view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+	
+    // tableView の frame は view.bounds 全体と仮定。他の UI 部品を配置する場合は要調整。
+    tableView = [[UITableView alloc] initWithFrame:view.bounds style:style];
+    tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    tableView.dataSource = self;
+    tableView.delegate = self;
+    [view addSubview:tableView];
+	
+    // 必要に応じて、ここで他の UI 部品を生成。
+	
+    self.view = view;
+}
+
+
+
+
+// そのまま。
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+// そのまま。
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+// そのまま。
+- (UITableViewCell *)tableView:(UITableView *)theTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+	
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+	
+    // cell の内容を設定。
+	
+    return cell;
+}
+
+
+// そのまま。
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 行が選択された場合の処理。
+}
+
+
+
+
 
 @end
