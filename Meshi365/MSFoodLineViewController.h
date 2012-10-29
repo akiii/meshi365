0cabf155770b1a6bf63eaa971d0f0140f45911d0
MlleIrene <mlleirenecahendanvers@gmail.com>
2012-10-28 22:21:58 -0700

    TodayViewControlerをTableViewControllerからViewControllerへ変更

--- Meshi365/MSFoodLineViewController.h
+++ Meshi365/MSFoodLineViewController.h
@@ -2,12 +2,12 @@
 //  MSFoodLineViewController.h
 //  Meshi365
 //
-//  Created by Mlle.Irene on 2012/10/29.
+//  Created by Mlle.Irene on 2012/10/28.
 //  Copyright (c) 2012年 Akifumi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MSFoodLineViewController : UITableViewController
+@interface MSFoodLineViewController : UIViewController
 
 @end
