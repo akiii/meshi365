eddc72cc5fe13b260a126d46d04cc62daccc0b5d
akiii <akiii@sfc.wide.ad.jp>
2012-10-29 15:13:20 +0900

    Added uuid controller class.

--- Meshi365/MSUIIDController.m
+++ Meshi365/MSUIIDController.m
@@ -10,4 +10,32 @@
 
 @implementation MSUIIDController
 
+- (BOOL)exist{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if ([defaults objectForKey:kUIID]) {
+        return YES;
+    }else {
+        return NO;
+    }
+}
+
+- (NSString *)uiid{
+    if ([self exist]) {
+        return [self uuid];
+    }else {
+        return [self create];
+    }
+}
+
+- (NSString *)create{
+    CFUUIDRef uuidObj = CFUUIDCreate(nil);
+    NSString *uuidString = (__bridge NSString*)CFUUIDCreateString(nil, uuidObj);
+    CFRelease(uuidObj);
+    return uuidString;
+}
+
+- (NSString *)uuid{
+    return [[NSUserDefaults standardUserDefaults] objectForKey:kUIID];
+}
+
 @end
