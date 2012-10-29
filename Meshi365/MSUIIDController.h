7b6b850af5679c67d462d5595b7ad03da95d08ec
akiii <akiii@sfc.wide.ad.jp>
2012-10-29 14:43:30 +0900

    Added a uiid class.

---
+++ Meshi365/MSUIIDController.h
@@ -0,0 +1,13 @@
+//
+//  MSUIIDController.h
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/29.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSUIIDController : NSObject
+
+@end
---
+++ Meshi365/MSUIIDController.m
@@ -0,0 +1,13 @@
+//
+//  MSUIIDController.m
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/29.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "MSUIIDController.h"
+
+@implementation MSUIIDController
+
+@end
