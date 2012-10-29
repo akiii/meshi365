7126d3d8bc35a542504751f0f8be0bb134665503
Tatsuya Hirose <kuroneko@wreathlitnoel.com>
2012-10-30 08:19:27 +0900

    MSValueImageViewController の追加

---
+++ Meshi365/ViewControllers/MSValueImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSValueImageViewController.h
+//  Meshi365
+//
+//  Created by tatsuya on 10/30/12.
+//  Copyright (c) 2012 Akifumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSValueImageViewController : UIViewController
+
+@end
---
+++ Meshi365/ViewControllers/MSValueImageViewController.m
@@ -0,0 +1,38 @@
+//
+//  MSValueImageViewController.m
+//  Meshi365
+//
+//  Created by tatsuya on 10/30/12.
+//  Copyright (c) 2012 Akifumi. All rights reserved.
+//
+
+#import "MSValueImageViewController.h"
+
+@interface MSValueImageViewController ()
+
+@end
+
+@implementation MSValueImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
