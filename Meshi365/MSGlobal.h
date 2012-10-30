cc0a086d9f68717d3b7d7e2981da788e66cd2be3
akiii <akiii@sfc.wide.ad.jp>
2012-10-31 01:58:48 +0900

    Added a header file.

---
+++ Meshi365/MSGlobal.h
@@ -0,0 +1,10 @@
+//
+//  MSGlobal.h
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/31.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#define URL_OF_POST_FOOD_PICTURE    @"http://aqueous-brushlands-6933.herokuapp.com/post/food_picture"
+#define URL_OF_FOOD_LINE_PICTURES   @"http://aqueous-brushlands-6933.herokuapp.com/food_pictures"
