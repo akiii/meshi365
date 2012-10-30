36f790f864f4a9da7ab45e3835b581a5b8221be1
akiii <akiii@sfc.wide.ad.jp>
2012-10-30 13:46:06 +0900

    Added was connector class.

---
+++ Meshi365/MSAWSConnector.m
@@ -0,0 +1,13 @@
+//
+//  MSAWSConnector.m
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/30.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "MSAWSConnector.h"
+
+@implementation MSAWSConnector
+
+@end
