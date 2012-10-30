a531a3df08d8ce2704777e29fa8ae16169a37d08
akiii <akiii@sfc.wide.ad.jp>
2012-10-31 06:48:58 +0900

    Modified a class value image view class.

--- Meshi365/ViewControllers/MSValueImageView.h
+++ Meshi365/ViewControllers/MSValueImageView.h
@@ -1,12 +1,15 @@
 #import <UIKit/UIKit.h>
+#import "MSFoodPictureImage.h"
+
+#define kNumOfStars 5
 
 @interface MSValueImageView : UIView{
-    UIButton *star0,*star1,*star2,*star3,*star4;
+    UIButton *star[kNumOfStars];
 }
 
 @property (nonatomic,strong) id delegate;
 @property (nonatomic       ) int cnt_stars;
-@property (nonatomic,strong) UIImage *original_image;
-@property (nonatomic,strong) UIImage *resized_image;
+@property (nonatomic,strong) UIImage *cameraImage;
+@property (nonatomic,strong) MSFoodPictureImage *squareFoodPictureImage;
 
 @end
--- Meshi365/ViewControllers/MSValueImageView.m
+++ Meshi365/ViewControllers/MSValueImageView.m
@@ -20,47 +20,23 @@
     
     self.cnt_stars = 3;
     
-    CGRect image_rect = CGRectMake(0, (self.original_image.size.height-self.original_image.size.width)/2,
-                                   self.original_image.size.width,
-                                   self.original_image.size.width);
-    self.resized_image = [UIImage imageWithCGImage:CGImageCreateWithImageInRect
-                            ([self.original_image CGImage], image_rect)];
+    CGRect image_rect = CGRectMake(0, (self.cameraImage.size.height-self.cameraImage.size.width)/2,
+                                   self.cameraImage.size.width,
+                                   self.cameraImage.size.width);
+    self.squareFoodPictureImage = [[MSFoodPictureImage alloc] initWithCGImage:CGImageCreateWithImageInRect([self.cameraImage CGImage], image_rect)];
     
-    UIImageView *im = [[UIImageView alloc] initWithImage:self.resized_image];
+    UIImageView *im = [[UIImageView alloc] initWithImage:self.squareFoodPictureImage];
     im.frame = CGRectMake(left_line, 30, 200, 200);
     [self addSubview:im];
 
-    
-    star0 = [UIButton buttonWithType:UIButtonTypeCustom];
-    star1 = [UIButton buttonWithType:UIButtonTypeCustom];
-    star2 = [UIButton buttonWithType:UIButtonTypeCustom];
-    star3 = [UIButton buttonWithType:UIButtonTypeCustom];
-    star4 = [UIButton buttonWithType:UIButtonTypeCustom];
-    
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    
-    star0.frame = CGRectMake(left_line,240,30,30);
-    star1.frame = CGRectMake(left_line+30,240,30,30);
-    star2.frame = CGRectMake(left_line+60,240,30,30);
-    star3.frame = CGRectMake(left_line+90,240,30,30);
-    star4.frame = CGRectMake(left_line+120,240,30,30);
-    
-    [star0 addTarget:self action:@selector(tap_star0:) forControlEvents:UIControlEventTouchDown];
-    [star1 addTarget:self action:@selector(tap_star1:) forControlEvents:UIControlEventTouchDown];
-    [star2 addTarget:self action:@selector(tap_star2:) forControlEvents:UIControlEventTouchDown];
-    [star3 addTarget:self action:@selector(tap_star3:) forControlEvents:UIControlEventTouchDown];
-    [star4 addTarget:self action:@selector(tap_star4:) forControlEvents:UIControlEventTouchDown];
-    
-    [self addSubview:star0];
-    [self addSubview:star1];
-    [self addSubview:star2];
-    [self addSubview:star3];
-    [self addSubview:star4];
-    
+    for (int i = 0; i < kNumOfStars; i++) {
+        star[i] = [UIButton buttonWithType:UIButtonTypeCustom];
+        star[i].frame = CGRectMake(left_line + 30 * i, 240, 30, 30);
+        star[i].tag = i;
+        [self setBackgroundImageOfStarButton:star[i].tag];
+        [star[i] addTarget:self action:@selector(tap_star0:) forControlEvents:UIControlEventTouchDown];
+        [self addSubview:star[i]];
+    }    
     
     UIButton *cancel_button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     cancel_button.frame = CGRectMake([[UIScreen mainScreen] bounds].size.width/2-85, 300, 80, 30);
@@ -77,45 +53,22 @@
     [self addSubview:save_button];
 }
 
--(void) tap_star0:(id)sender{
-    self.cnt_stars = 1;
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
+-(void) tap_star0:(UIButton *)sender{
+    self.cnt_stars = sender.tag + 1;
+    for (int i = 0; i < kNumOfStars; i++) {
+        [self setBackgroundImageOfStarButton:star[i].tag];
+    }
+    self.squareFoodPictureImage.foodPicture.starNum = self.cnt_stars;
 }
--(void) tap_star1:(id)sender{
-    self.cnt_stars = 2;
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-}
--(void) tap_star2:(id)sender{
-    self.cnt_stars = 3;
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-}
--(void) tap_star3:(id)sender{
-    self.cnt_stars = 4;
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"starNonSelect.png"] forState:UIControlStateNormal];
-}
--(void) tap_star4:(id)sender{
-    self.cnt_stars = 5;
-    [star0 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star1 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star2 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star3 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
-    [star4 setBackgroundImage:[UIImage imageNamed:@"star.png"] forState:UIControlStateNormal];
+
+- (void)setBackgroundImageOfStarButton:(int)tag{
+    UIImage *backgroundImage;
+    if (tag < self.cnt_stars) {
+        backgroundImage = [UIImage imageNamed:@"star.png"];
+    }else {
+        backgroundImage = [UIImage imageNamed:@"starNonSelect.png"];
+    }
+    [star[tag] setBackgroundImage:backgroundImage forState:UIControlStateNormal];
 }
 
 @end
