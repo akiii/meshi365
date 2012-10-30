3ad5aa5183595bf63b998df92ae748e4d4e569e9
akiii <akiii@sfc.wide.ad.jp>
2012-10-30 14:53:00 +0900

    Added food picture model class.

---
+++ Meshi365/Models/MSFoodPicture.h
@@ -0,0 +1,13 @@
+//
+//  MSFoodPicture.h
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/30.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSFoodPicture : NSObject
+
+@end
