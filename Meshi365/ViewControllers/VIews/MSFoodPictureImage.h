ecd5259b3dbfc895e30712c6ffeccfa8f7da2c11
akiii <akiii@sfc.wide.ad.jp>
2012-10-31 04:41:10 +0900

    Added a food picture image class.

---
+++ Meshi365/ViewControllers/VIews/MSFoodPictureImage.h
@@ -0,0 +1,14 @@
+//
+//  MSFoodPictureImage.h
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/31.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MSFoodPicture.h"
+
+@interface MSFoodPictureImage : UIImage
+@property (nonatomic, strong) MSFoodPicture *foodPicture;
+@end
---
+++ Meshi365/ViewControllers/VIews/MSFoodPictureImage.m
@@ -0,0 +1,13 @@
+//
+//  MSFoodPictureImage.m
+//  Meshi365
+//
+//  Created by Akifumi on 2012/10/31.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "MSFoodPictureImage.h"
+
+@implementation MSFoodPictureImage
+
+@end
