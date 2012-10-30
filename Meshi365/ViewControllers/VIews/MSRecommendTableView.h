2204726e42d562a0f6231effb5cf9958819c8fea
MlleIrene <mlleirenecahendanvers@gmail.com>
2012-10-30 15:22:40 -0700

    リコメンド画面のTableViewを追加

---
+++ Meshi365/ViewControllers/VIews/MSRecommendTableView.h
@@ -0,0 +1,13 @@
+//
+//  MSRecommendTableView.h
+//  Meshi365
+//
+//  Created by Mlle.Irene on 2012/10/30.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSRecommendTableView : UITableView
+
+@end
---
+++ Meshi365/ViewControllers/VIews/MSRecommendTableView.m
@@ -0,0 +1,31 @@
+//
+//  MSRecommendTableView.m
+//  Meshi365
+//
+//  Created by Mlle.Irene on 2012/10/30.
+//  Copyright (c) 2012年 Akifumi. All rights reserved.
+//
+
+#import "MSRecommendTableView.h"
+
+@implementation MSRecommendTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
