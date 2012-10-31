b2ea4a0be789b7969cf31c12618cd039b50d94bf
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 07:47:17 +0900

    Added get requesting users macro.

--- Meshi365/MSGlobal.h
+++ Meshi365/MSGlobal.h
@@ -6,8 +6,9 @@
 //  Copyright (c) 2012年 Akifumi. All rights reserved.
 //
 
-#define URL_OF_SIGN_UP              @"http://aqueous-brushlands-6933.herokuapp.com/api/signup"
-#define URL_OF_GET_FRIENDS(uiid)    [NSString stringWithFormat:@"http://aqueous-brushlands-6933.herokuapp.com/api/%@/friends", uiid]
+#define URL_OF_SIGN_UP                      @"http://aqueous-brushlands-6933.herokuapp.com/api/signup"
+#define URL_OF_GET_FRIENDS(uiid)            [NSString stringWithFormat:@"http://aqueous-brushlands-6933.herokuapp.com/api/%@/friends", uiid]
+#define URL_OF_GET_REQUESTING_USERS(uiid)   [NSString stringWithFormat:@"http://aqueous-brushlands-6933.herokuapp.com/api/%@/requesting_users", uiid]
 
-#define URL_OF_POST_FOOD_PICTURE    @"http://aqueous-brushlands-6933.herokuapp.com/api/post/food_picture"
-#define URL_OF_FOOD_LINE_PICTURES   @"http://aqueous-brushlands-6933.herokuapp.com/food_pictures"
+#define URL_OF_POST_FOOD_PICTURE            @"http://aqueous-brushlands-6933.herokuapp.com/api/post/food_picture"
+#define URL_OF_FOOD_LINE_PICTURES           @"http://aqueous-brushlands-6933.herokuapp.com/food_pictures"
