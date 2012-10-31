822087da8a2137e2af37c661cbd85f8b4798c716
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 06:28:13 +0900

    Changed a url.

--- Meshi365/MSGlobal.h
+++ Meshi365/MSGlobal.h
@@ -6,5 +6,5 @@
 //  Copyright (c) 2012年 Akifumi. All rights reserved.
 //
 
-#define URL_OF_POST_FOOD_PICTURE    @"http://aqueous-brushlands-6933.herokuapp.com/post/food_picture"
+#define URL_OF_POST_FOOD_PICTURE    @"http://aqueous-brushlands-6933.herokuapp.com/api/post/food_picture"
 #define URL_OF_FOOD_LINE_PICTURES   @"http://aqueous-brushlands-6933.herokuapp.com/food_pictures"
