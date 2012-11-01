7e09941c8493b21ba459e21ed7b1f4b6f8247210
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 09:55:00 +0900

    Added comment outs.

--- Meshi365/MSGlobal.h
+++ Meshi365/MSGlobal.h
@@ -6,9 +6,11 @@
 //  Copyright (c) 2012年 Akifumi. All rights reserved.
 //
 
+/* keys : { "name" , "uiid" , "profile_image_url" } */
 #define URL_OF_SIGN_UP                      @"http://aqueous-brushlands-6933.herokuapp.com/api/signup"
 #define URL_OF_GET_FRIENDS(uiid)            [NSString stringWithFormat:@"http://aqueous-brushlands-6933.herokuapp.com/api/%@/friends", uiid]
 #define URL_OF_GET_REQUESTING_USERS(uiid)   [NSString stringWithFormat:@"http://aqueous-brushlands-6933.herokuapp.com/api/%@/requesting_users", uiid]
+/* keys : { "from_user_uiid" , "to_user_uiid" } */
 #define URL_OF_SEND_FRIEND_REQUEST          @"http://aqueous-brushlands-6933.herokuapp.com/api/send_friend_request"
 
 #define URL_OF_POST_FOOD_PICTURE            @"http://aqueous-brushlands-6933.herokuapp.com/api/post/food_picture"
