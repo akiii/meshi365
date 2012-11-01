fc272c533ea2ed2611b3c4b0625e7827cb91fc9f
akiii <akiii@sfc.wide.ad.jp>
2012-11-02 07:17:39 +0900

    Modified server url macro.

--- Meshi365/MSGlobal.h
+++ Meshi365/MSGlobal.h
@@ -6,14 +6,33 @@
 //  Copyright (c) 2012年 Akifumi. All rights reserved.
 //
 
+#define LOCAL       0
+#define STAGING     1
+#define RELEASE     2
+
+#define ENVIRONMENT STAGING
+
+#if ENVIRONMENT == LOCAL
+
+#define SERVER_BASE_URL @"http://localhost:3000"
+
+#elif ENVIRONMENT == STAGING
+
+#define SERVER_BASE_URL @"http://morning-depths-1177.herokuapp.com"
+
+#elif ENVIRONMENT == RELEASE
+
+#define SERVER_BASE_URL @"http://morning-depths-1177.herokuapp.com"
+
+#endif
+
 /* params keys : { "name" , "uiid" , "profile_image_url" } */
-#define URL_OF_SIGN_UP                      @"http://morning-depths-1177.herokuapp.com/api/signup"
-#define URL_OF_GET_FRIENDS(uiid)            [NSString stringWithFormat:@"http://morning-depths-1177.herokuapp.com/api/%@/friends", uiid]
-#define URL_OF_GET_REQUESTING_USERS(uiid)   [NSString stringWithFormat:@"http://morning-depths-1177.herokuapp.com/api/%@/requesting_users", uiid]
+#define URL_OF_SIGN_UP                      [NSString stringWithFormat:@"%@/api/signup", SERVER_BASE_URL]
+#define URL_OF_GET_FRIENDS(uiid)            [NSString stringWithFormat:@"%@/api/%@/friends", SERVER_BASE_URL, uiid]
+#define URL_OF_GET_REQUESTING_USERS(uiid)   [NSString stringWithFormat:@"%@/api/%@/requesting_users", SERVER_BASE_URL, uiid]
 /* params keys : { "from_user_uiid" , "to_user_uiid" } */
-#define URL_OF_SEND_FRIEND_REQUEST          @"http://morning-depths-1177.herokuapp.com/api/send_friend_request"
+#define URL_OF_SEND_FRIEND_REQUEST          [NSString stringWithFormat:@"%@/api/send_friend_request", SERVER_BASE_URL]
 
 /* params keys : { "uiid" , "url" , "store_name" , "menu_name", "menity" , "comment" , "star_num" } */
-#define URL_OF_POST_FOOD_PICTURE            @"http://morning-depths-1177.herokuapp.com/api/post/food_picture"
-#define URL_OF_FOOD_LINE_PICTURES           @"http://morning-depths-1177.herokuapp.com/food_pictures"
-#define URL_OF_FOOD_LINE(uiid)              [NSString stringWithFormat:@"http://morning-depths-1177.herokuapp.com/api/%@/food_line", uiid]
+#define URL_OF_POST_FOOD_PICTURE            [NSString stringWithFormat:@"%@/api/post/food_picture", SERVER_BASE_URL]
+#define URL_OF_FOOD_LINE(uiid)              [NSString stringWithFormat:@"%@/api/%@/food_line", SERVER_BASE_URL, uiid]
