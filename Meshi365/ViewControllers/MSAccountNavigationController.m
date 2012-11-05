a003456bfcb1d1e9cb15a03986e90ce77e976a6c
Tatsuya Hirose <kuroneko@wreathlitnoel.com>
2012-11-05 17:49:30 +0900

    Add MSAccountNavigationController

---
+++ Meshi365/ViewControllers/MSAccountNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  MSAccountNavigationController.m
+//  Meshi365
+//
+//  Created by tatsuya on 11/5/12.
+//  Copyright (c) 2012 Akifumi. All rights reserved.
+//
+
+#import "MSAccountNavigationController.h"
+
+@interface MSAccountNavigationController ()
+
+@end
+
+@implementation MSAccountNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
