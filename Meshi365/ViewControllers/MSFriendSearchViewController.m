bdcf557539c177cbd0091aae9f78399418996a35
Tatsuya Hirose <kuroneko@wreathlitnoel.com>
2012-11-05 17:09:50 +0900

    Add MSFriendSearchViewController

---
+++ Meshi365/ViewControllers/MSFriendSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  MSFriendSearchViewController.m
+//  Meshi365
+//
+//  Created by tatsuya on 11/5/12.
+//  Copyright (c) 2012 Akifumi. All rights reserved.
+//
+
+#import "MSFriendSearchViewController.h"
+
+@interface MSFriendSearchViewController ()
+
+@end
+
+@implementation MSFriendSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
